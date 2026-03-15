d13464d7c1453e09d6279bb36df8bb2c019c54d3
Duy <gochoctapcuatuanduy@gmail.com>
2026-03-15 13:18:20 +0700

    Delete old project

--- ASP_DOTNET_MVC/ASP_DOTNET_MVC/IRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ASP_DOTNET_MVC
-{
-    public interface IRepository
-    {
-        string GetById(string id);
-    }
-}
--- ASP_DOTNET_MVC/ASP_DOTNET_MVC/Models/HelloModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ASP_DOTNET_MVC.Models
-{
-    public class HelloModel
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-    }
-}
