1aad3ff8d78b2ecf5f4548c2b46144cde0f525b8
Duy <gochoctapcuatuanduy@gmail.com>
2026-03-18 16:04:58 +0700

    First Practice with Book ASP .NET Core in Action

---
+++ LearnViaActionBook/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace LearnViaActionBook
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+    }
+
+
+}
