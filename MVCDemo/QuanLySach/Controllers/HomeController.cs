2a5aeb4bc15a42e7ac2793ae2861c2bc87ca859c
Duy <gochoctapcuatuanduy@gmail.com>
2026-03-23 09:58:29 +0700

    practice: ASP.NET MVC

---
+++ MVCDemo/QuanLySach/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using QuanLySach.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLySach.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBIO db = new DBIO();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<SACH> list = db.getList_SACH();
+            return View(list);
+        }
+
+        //Không dùng tham số gì cả cho Create()
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //Khi gọi đến phương thức addObject_SACH(SACH s) in DBIO.cs
+        //thì đồng thời ta cần một thông điệp nào đó để nó load dữ liệu lên
+        //=> dùng [HttpPost]
+        [HttpPost]
+        public ActionResult Create(SACH s)
+        {
+            db.addObject_SACH(s);
+            //sau khi thêm rồi, ta điều hướng người dùng đến trang Index
+            return RedirectToAction("Index");
+        }
+        //Sửa sách phức tạp hơn thêm (create) vì ta cần biết sửa sách nào?
+        //Ta cần biết id của quyển sách bạn đang định sửa
+        public ActionResult Edit(string id)
+        {
+            SACH s = db.getObject_SACH(id);
+            return View(s); //lấy ra quyển sách thì sau đó mới sửa được
+        }
+        [HttpPost] //Ý nghĩa là khi bạn gọi Edit nó cần gửi lên thông điệp để sửa thông tin này
+        public ActionResult Edit(SACH s) //phải đặt tên là Edit như hàm bên trên, không dùng tên khác
+        {
+            db.editObject_SACH(s);
+            return RedirectToAction("Index"); //Khi sửa rồi thì hệ thống sẽ điều hướng quay về trang chủ
+        }
+        //Xóa một quyển sách
+        //Ta cần biết id của quyển sách bạn đang định xóa
+        public ActionResult Delete(string id)
+        {
+            SACH s = db.getObject_SACH(id);
+            return View(s); //lấy ra quyển sách thì sau đó mới xóa được
+        }
+        [HttpPost] //Ý nghĩa là khi bạn gọi Delete nó cần gửi lên thông điệp để xóa thông tin này
+        public ActionResult Delete(SACH s)
+        {
+            db.deleteObject_SACH(s);
+            return RedirectToAction("Index"); //Khi xóa rồi thì hệ thống sẽ điều hướng quay về trang chủ
+        }
+        //Xem một quyển sách
+        //Ta cần biết id của quyển sách bạn đang định xem chi tiết
+        public ActionResult Details(string id)
+        {
+            SACH s = db.getObject_SACH(id);
+            return View(s); //lấy ra quyển sách cần xem
+        }
+
+        
+    }
+}
---
+++ MVCDemo/QuanLySach/Models/DBIO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLySach.Models
+{
+    public class DBIO
+    {
+        QLSACHEntities1 mydb = new QLSACHEntities1();
+        //hàm getList_SACH() để thực hiện một câu truy vấn
+        //trả về một danh sách dùng iQuery
+        public List<SACH> getList_SACH()
+        {
+            //access to the SACH table to read all rows
+            return mydb.SACHes.ToList();
+        }
+        //hàm getObject_SACH(string id) trả về một quyển sách theo id
+        public SACH getObject_SACH(string id)
+        {
+            //biểu thức so sánh có cú pháp như C#
+            //chứ không phải của SQL
+            return mydb.SACHes.Where(c => c.MASACH == id).FirstOrDefault();
+        }
+        //hàm addObject_SACH(SACH s) thêm vào một quyển sách
+        public void addObject_SACH(SACH s)
+        {
+            mydb.SACHes.Add(s);
+            mydb.SaveChanges();
+        }
+        //hàm editObject_SACH(SACH s) sửa một quyển sách
+        public void editObject_SACH(SACH s)
+        {
+            SACH x = getObject_SACH(s.MASACH);
+            x.TENSACH = s.TENSACH;
+            x.TACGIA = s.TACGIA;
+            x.NAMXB = s.NAMXB;
+            x.SOLUONG = s.SOLUONG;
+            x.IMAGECOVER = s.IMAGECOVER;
+            mydb.SaveChanges();
+        }
+        //hàm deleteObject_SACH(SACH s) xóa một quyển sách s
+        public void deleteObject_SACH(SACH s)
+        {
+            SACH x = getObject_SACH(s.MASACH);
+            mydb.SACHes.Remove(x);
+            mydb.SaveChanges();
+        }
+
+    }
+}
+
