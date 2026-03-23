cf769fb9dfe9e87178eb424748634a5591bbdb62
Duy <gochoctapcuatuanduy@gmail.com>
2026-03-23 11:27:07 +0700

    new

---
+++ MVCDemo/TH_BT2/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH_BT2.Models;
+
+namespace TH_BT2.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBIO db = new DBIO();
+
+        public ActionResult Index()
+        {
+            List<NHANVIEN> list = db.getList_NHANVIEN();
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(NHANVIEN nv) {
+            db.addObj(nv);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(string id)
+        {
+            NHANVIEN s = db.getObj_NHANVIEN(id);
+            return View(s);
+        }
+        [HttpPost]
+        public ActionResult Edit(NHANVIEN newNV)
+        {
+            db.editObj(newNV);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string id)
+        {
+            db.getObj_NHANVIEN(id);
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Delete(NHANVIEN nv)
+        {
+            db.delObj(nv);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details (string id)
+        {
+            NHANVIEN nv= db.getObj_NHANVIEN(id);
+            return View(nv);
+        }
+    }
+}
---
+++ MVCDemo/TH_BT2/Models/DBIO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TH_BT2.Models
+{
+    public class DBIO
+    {
+        MaSV_tenSVEntities1 mdb = new MaSV_tenSVEntities1();
+
+        public List<NHANVIEN> getList_NHANVIEN()
+        {
+            return mdb.NHANVIENs.ToList();
+        }
+
+        public NHANVIEN getObj_NHANVIEN(string id)
+        {
+            return mdb.NHANVIENs.Where(t => t.MANHANVIEN.Equals(id)).FirstOrDefault();
+        }
+
+        public void addObj(NHANVIEN nv)
+        {
+            mdb.NHANVIENs.Add(nv);
+            mdb.SaveChanges();
+        }
+
+        public void editObj(NHANVIEN newnv)
+        {
+            NHANVIEN  nv = getObj_NHANVIEN(newnv.MANHANVIEN);
+            nv.TENNHANVIEN = newnv.TENNHANVIEN;
+            nv.NGAYSINH = newnv.NGAYSINH;
+            nv.GIOITINH= newnv.GIOITINH;
+            nv.EMAIL= newnv.EMAIL;
+            nv.DIENTHOAI = newnv.DIENTHOAI;
+            nv.PROFILE = newnv.PROFILE;
+            mdb.SaveChanges();
+        }
+        public void delObj(NHANVIEN nv)
+        {
+            NHANVIEN x = getObj_NHANVIEN(nv.MANHANVIEN);
+            mdb.NHANVIENs.Remove(x);    
+            mdb.SaveChanges();
+        }
+     }
+}
