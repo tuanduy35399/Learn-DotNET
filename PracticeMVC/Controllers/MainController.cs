da48e57533219b0f42a03dca4090bf4d38c61fe7
Duy <gochoctapcuatuanduy@gmail.com>
2026-03-27 15:57:13 +0700

    Create 2 practice

---
+++ PracticeMVC/Controllers/MainController.cs
@@ -0,0 +1,60 @@
+﻿using PracticeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeMVC.Controllers
+{
+    public class MainController : Controller
+    {
+        DBIO db = new DBIO();
+        public ActionResult Index()
+        {
+            List <SINHVIEN> list = db.getList_SinhVien();
+            return View(list);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(SINHVIEN sv)
+        {
+            db.add_SinhVien(sv);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(string id)
+        {
+            SINHVIEN sv = db.getDetail_SinhVien(id);
+            return View(sv);
+
+        }
+
+        [HttpPost] 
+        public ActionResult Edit(SINHVIEN sv)
+        {
+            db.update_SinhVien(sv);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string id)
+        {
+            SINHVIEN sv = db.getDetail_SinhVien(id);
+            return View(sv);
+        }
+        [HttpPost]
+        public ActionResult Delete(SINHVIEN sv)
+        {
+            db.delete_SinhVien(sv);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(string id)
+        {
+            SINHVIEN s= db.getDetail_SinhVien(id);
+            return View(s);
+        }
+            
+    }
+}
---
+++ PracticeMVC/Models/DBIO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticeMVC.Models
+{
+    public class DBIO
+    {
+        QLHOCVIENEntities1 myDataBase = new QLHOCVIENEntities1();
+        public List<SINHVIEN> getList_SinhVien()
+        {
+            return myDataBase.SINHVIENs.ToList();
+        }
+        public SINHVIEN getDetail_SinhVien(string id)
+        {
+            return myDataBase.SINHVIENs.Where(element => element.MAHOCVIEN.Equals(id)).FirstOrDefault();
+        }
+        public void add_SinhVien(SINHVIEN sinhvien)
+        {
+            myDataBase.SINHVIENs.Add(sinhvien);
+            myDataBase.SaveChanges();
+        }
+        public void update_SinhVien(SINHVIEN newSinhVien)
+        {
+            SINHVIEN oldSinhVien = getDetail_SinhVien(newSinhVien.MAHOCVIEN);
+            oldSinhVien.MAHOCVIEN = newSinhVien.MAHOCVIEN;
+            oldSinhVien.TENHOCVIEN = newSinhVien.TENHOCVIEN;
+            oldSinhVien.NGAYSINH = newSinhVien.NGAYSINH;
+            oldSinhVien.DIENTHOAI = newSinhVien.DIENTHOAI;
+            oldSinhVien.EMAIL = newSinhVien.EMAIL;
+            oldSinhVien.PROFILE = newSinhVien.PROFILE;
+            oldSinhVien.GIOITINH = newSinhVien.GIOITINH;
+            myDataBase.SaveChanges();
+
+        }
+        public void delete_SinhVien(SINHVIEN sinhVien)
+        {
+            SINHVIEN oldSinhVien = getDetail_SinhVien(sinhVien.MAHOCVIEN);
+            myDataBase.SINHVIENs.Remove(oldSinhVien);
+            myDataBase.SaveChanges();
+        }
+    }
+}
