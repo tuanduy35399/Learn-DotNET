bfba6aec80c4cd6e5e62d9ab4be46e7156c1ffa8
Duy <gochoctapcuatuanduy@gmail.com>
2026-03-28 12:28:05 +0700

    Practice Day 3: MinimalAPI

--- MinimalAPI/MyTodoApp/Program.cs
+++ MinimalAPI/MyTodoApp/Program.cs
@@ -1,6 +1,33 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpLogging(opts =>
+    opts.LoggingFields = HttpLoggingFields.RequestProperties);
+builder.Logging.AddFilter(
+    "Microsoft.AspNetCore.HttpLogging", LogLevel.Information
+);
 var app = builder.Build();
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseDeveloperExceptionPage();
+// }
+app.UseStaticFiles();
+app.UseHttpLogging();
+app.UseWelcomePage();
+// app.MapGet("/", () =>
+// {
+//     throw new Exception("This is a test exception.");
+// });
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/todo", () =>
+{
 
+    List<Todo> todos = new List<Todo>
+    {
+        new Todo {Id= 1, Description= "Hello" },
+        new Todo {Id= 2, Description= "Hi"}
+    };
+    return todos;
+});
 app.Run();
---
+++ MinimalAPI/MyTodoApp/Todo.cs
@@ -0,0 +1,4 @@
+public record Todo {
+    public int Id { get; set; }
+    public string Description { get; set; } = string.Empty; 
+}
