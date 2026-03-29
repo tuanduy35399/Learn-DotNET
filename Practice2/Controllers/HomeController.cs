718ea7c0ea6b0ffc3d191da057a4b7c49a9eb799
Duy <gochoctapcuatuanduy@gmail.com>
2026-03-30 01:02:42 +0700

    Practice2

---
+++ Practice2/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Practice2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practice2.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBIO db = new DBIO();
+        public ActionResult Index()
+        {
+            //ViewBag.Whoareyou = "Bạn là ai";
+            //ViewBag.AnswerText = "Nhập tên bạn vào đây";
+            ViewBag.LabelTimKiem = "Tìm kiếm ";
+            List<SINHVIEN> ds = db.getList_HV();
+            ViewData["TimKiem"] = new SelectList(ds, "MAHOCVIEN", "TENHOCVIEN");
+            List<SINHVIEN> listSV = db.getList_HV();
+            return View(listSV);
+        }
+
+        public ActionResult TimKiem(string id)
+        {
+            ViewBag.LabelTimKiem = "Tìm kiếm ";
+            List<SINHVIEN> ds = db.getList_HV();
+            ViewData["TimKiem"] = new SelectList(ds, "MAHOCVIEN", "TENHOCVIEN");
+            SINHVIEN sv = db.getDetail(id);
+            List<SINHVIEN> kq= new List<SINHVIEN>();
+            if(sv!=null)
+            {
+                kq.Add(sv);
+            }
+
+            return View("Index", kq);
+
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(SINHVIEN sv)
+        {
+            db.createSV(sv);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(string id)
+        {
+            SINHVIEN sv = db.getDetail(id);
+            return View( sv);
+        }
+        [HttpPost]
+        public ActionResult Edit(SINHVIEN newSV)
+        {
+            db.upadateSV(newSV);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string id)
+        {
+            SINHVIEN sv = db.getDetail(id);
+            return View(sv);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirm(string id)
+        {
+            db.deleteSV(id);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(string id)
+        {
+            SINHVIEN sv= db.getDetail(id);
+            return View(sv);
+        }
+    }
+}
---
+++ Practice2/Models/DBIO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice2.Models
+{
+    public class DBIO
+    {
+        QLHOCVIENEntities1 mdb = new QLHOCVIENEntities1();
+
+        public List<SINHVIEN> getList_HV()
+        {
+            return mdb.SINHVIENs.ToList();
+        }
+        public SINHVIEN getDetail(string id)
+        {
+            return mdb.SINHVIENs.Find(id);
+
+        }
+        public void createSV(SINHVIEN sv)
+        {
+            mdb.SINHVIENs.Add(sv);
+            mdb.SaveChanges();
+        }
+        public void upadateSV(SINHVIEN newSV)
+        {
+            SINHVIEN sv = getDetail(newSV.MAHOCVIEN);
+            if(sv!=null)
+            {
+                mdb.Entry(sv).CurrentValues.SetValues(newSV);
+            }
+            mdb.SaveChanges();
+
+        }
+        public void deleteSV(string id)
+        {
+            SINHVIEN sv = mdb.SINHVIENs.Find(id);
+            mdb.SINHVIENs.Remove(sv);
+            mdb.SaveChanges();
+        }
+    }
+}
