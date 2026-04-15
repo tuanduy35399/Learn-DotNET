45cad9781cc1c9f0aede0b4ed6d839ca6497d096
Duy <gochoctapcuatuanduy@gmail.com>
2026-04-15 16:32:46 +0700

    Day3 learn book API

--- LearnViaBook/MyBGList/Program.cs
+++ LearnViaBook/MyBGList/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Cors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(cfg =>
+    {
+        //cfg.AllowAnyOrigin();//cái này chỉ đơn giản là vô hiệu hóa chính sách đồng nguồn
+        //(same origin policy- SOP)
+        //nó mất đi lớp bảo vệ
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]); //giới hạn những domain nằm trong file appsettings.json
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    opt.AddPolicy(name: "AnyOrigin", //tạo 1 policy custom 
+    cfg =>
+    {
+        cfg.AllowAnyOrigin();
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,14 +49,18 @@ else
 {
     app.UseExceptionHandler("/error");
 }
-app.MapGet("/error", () => Results.Problem());
-//app.MapGet("/error/test", () => { 
-//    throw new Exception("test"); 
-//});
-app.MapGet("/error/test", () => { throw new Exception("test"); });
-//app.UseWelcomePage();
+app.MapGet("/error",[EnableCors("AnyOrigin")] () => Results.Problem());
+
+app.MapGet("/error/test", [EnableCors("AnyOrigin")] () => { throw new Exception("test"); });
+//Minimal API có lợi thế trong việc áp dụng chính sách custom cho 1 endpoint cụ thể
+//ngược lại nếu dùng Controller thay vì minimal API thì chỉ có cách khai báo toàn cục áp dụng chính 
+//sách cho tất cả endpoint 
+//app.MapControllers().RequireCors("AnyOrigin");
+//[EnableCors] xử lý preflight tốt hơn -> ta sẽ ưu tiên dùng
 app.UseHttpsRedirection();
 
+app.UseCors();
+//app.UseCors("AnyOrigin"); //nếu muốn gọi custom cfg
 app.UseAuthorization();
 
 app.MapControllers();
