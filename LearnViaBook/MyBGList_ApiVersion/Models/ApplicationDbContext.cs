50e5ecd0445176468351723c0860b3992862151b
Duy <gochoctapcuatuanduy@gmail.com>
2026-04-19 06:27:44 +0700

    learn ef core

---
+++ LearnViaBook/MyBGList_ApiVersion/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MyBGList_ApiVersion.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt) : base(opt) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // TODO: custom code here
+        }
+
+    }
+
+}
