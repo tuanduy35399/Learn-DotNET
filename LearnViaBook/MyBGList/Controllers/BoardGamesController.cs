10d6750d9946c675a6965e99cdb44e944c5d6a0a
Duy <gochoctapcuatuanduy@gmail.com>
2026-04-25 23:23:12 +0700

    update: complete paging,sorting,filtering

--- LearnViaBook/MyBGList/Controllers/BoardGamesController.cs
+++ LearnViaBook/MyBGList/Controllers/BoardGamesController.cs
@@ -4,6 +4,7 @@ using MyBGList.DTO;
 using System.Xml.Linq;
 using MyBGList.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
 namespace MyBGList.Controllers
 {
     [Route("[controller]")] //endpoint là tên của Controller bỏ đi cụm Controller => /BoardGames
@@ -21,16 +22,36 @@ namespace MyBGList.Controllers
         }
         //Đây là REST API Chuẩn HATEOAS
         [HttpGet(Name = "GetBoardGames")]
-        public async Task<RestDTO<BoardGame[]>> Get() 
+        [ResponseCache(Location = ResponseCacheLocation.Any, Duration =60)]
+        public async Task<RestDTO<BoardGame[]>> Get(
+            int pageIndex=0, 
+            int pageSize=10, 
+            string? sortColumn = "Name",
+            string? sortOrder = "ASC",
+            string? filterQuery=null
+            )
             {
-            var query = _context.BoardGames;
+
+            var query = _context.BoardGames.AsQueryable(); //AsQueryable giúp biến query thành IQueryable
+            //có thể tiếp tục build và xử lý bên DB thay vì RAM
+            if (!string.IsNullOrEmpty(filterQuery))
+                query.Where(b => b.Name.Contains(filterQuery));
+            var recordCount = await query.CountAsync();
+            query= query
+                .OrderBy($"{sortColumn} {sortOrder}") //dùng dynamic LINQ thay cho lamda expression
+                //phải có dấu cách ở giữa
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize);
             return new RestDTO<BoardGame[]>() 
             {
             //Data = new BoardGame[] { //Thay vì tạo 1 instance mới như vầy thì ta dùng Dependency Injection
-            Data= await query.ToArrayAsync(), 
+            Data= await query.ToArrayAsync(),
+            PageIndex = pageIndex,
+            PageSize = pageSize, 
+            RecordCount= recordCount,
             Links = new List<LinkDTO> { 
             new LinkDTO(
-            Url.Action(null, "BoardGames", null, Request.Scheme)!,
+            Url.Action(null, "BoardGames", new {pageIndex,pageSize}, Request.Scheme)!,
             "self",
             "GET"),
             }
